d0bec9bed6701ef44778c3795f9f1202f5dcae3e
resist15 <souravhjagtap21@gmail.com>
2026-09-06 17:12:12 +0530

    retro-service: Implement Google OAuth base
    - Implement Google Oauth base still internal service registration and
      login is pending for this need to add new column in user table to have
      provider type

--- app/routers/google_auth.py
+++ app/routers/google_auth.py
@@ -1,7 +1,10 @@
 from authlib.integrations.starlette_client import OAuth
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.requests import Request
 
 from app.core.config import settings
+from app.dependencies.user import get_user_service
+from app.service.user import UserService
 
 oauth = OAuth()
 
@@ -13,4 +16,19 @@ oauth.register(
     server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
 )
 
-router = APIRouter("/auth/google")
+google_router = APIRouter(prefix="/auth/google")
+
+
+@google_router.get("/login")
+async def google_login_redirect(request: Request):
+    callback_url = request.url_for("callback_google")
+    return await oauth.google.authorize_redirect(request, callback_url)
+
+
+@google_router.get("/callback", name="callback_google")
+async def google_callback(
+    request: Request, user_service: UserService = Depends(get_user_service)
+):
+    token = await oauth.google.authorize_access_token(request)
+    user = token["userinfo"]
+    return dict(user)
--- app/routers/public_router.py
+++ app/routers/public_router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from app.routers.auth import auth_router
+from app.routers.google_auth import google_router
 
 public_router = APIRouter()
 
 public_router.include_router(auth_router)
+public_router.include_router(google_router)
