ce07c7247f0bb26896d28ef2bb7023d26aef1bd9
resist15 <souravhjagtap21@gmail.com>
2026-09-10 07:35:27 +0530

    retro-service: Implement small tracks fetch api

--- app/repository/music.py
+++ app/repository/music.py
@@ -1,15 +1,36 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import load_only, selectinload
 
-from app.model.music import Track
+from app.model.music import Album, Artist, Track
 
 
 class MusicRepository:
     def __init__(self, session: AsyncSession):
         self.db = session
 
-    async def get_tracks(self) -> list[Track] | None:
-        q = select(Track)
+    async def get_tracks(self) -> list[Track]:
+        q = select(Track).options(
+            load_only(
+                Track.id,
+                Track.title,
+                Track.album_id,
+                Track.release_date,
+                Track.bitrate,
+                Track.sample_rate,
+                Track.file_extension,
+                Track.duration_secs,
+            ),
+            selectinload(Track.album).load_only(
+                Album.id,
+                Album.name,
+            ),
+            selectinload(Track.artists).load_only(
+                Artist.id,
+                Artist.name,
+            ),
+        )
+
         result = await self.db.execute(q)
-        music = result.scalars().all()
-        return music
+
+        return list(result.scalars().all())
