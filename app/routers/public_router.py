d90877b2cc2253d3297631d0a649c641582b9834
resist15 <souravhjagtap21@gmail.com>
2026-09-22 23:02:53 +0530

    retro-service: Add Docker compose for deployment

--- app/routers/public_router.py
+++ app/routers/public_router.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from app.routers.auth import auth_router
 from app.routers.google_auth import google_router
+from app.routers.health import health_router
 
 public_router = APIRouter()
 
 public_router.include_router(auth_router)
 public_router.include_router(google_router)
+public_router.include_router(health_router)
